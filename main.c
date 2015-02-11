b9def757a4cdff0f050b9b117b964cc16ed9dd06
my <as@as.com>
2015-02-11 10:50:10 -0800

    main.c: added greeting with command line parameter

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include "stdio.h"
 
 int main(int argc, char* argv[]) {
-	printf("Hello, world!");
+	if (argc>1) {
+		printf("Hello, %s!",argv[1]);
+	} else {
+		printf("Hello, %s!",argv[0]);
+	}
 	return 0;
-}
+} 
